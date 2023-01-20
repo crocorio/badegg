39a65bba99f0c1e22f03fbb27d24bf76e1302fd6
crocorio <miyakonojououdo@gmail.com>
2023-01-20 12:22:06 +0900

    created an edit function for the items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.all.order('id DESC')
   end
@@ -20,6 +20,17 @@ class ItemsController < ApplicationController
 
   def show
   end 
+
+  def edit
+  end 
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
 
 
   private
